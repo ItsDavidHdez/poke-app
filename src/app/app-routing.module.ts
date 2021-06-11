2363024d121022fb5780d47226e5c2a2d97024df
ItsDavidHdez <david-0610@live.com.mx>
2021-06-11 16:09:23 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'characterslist',
+    loadChildren: () => import('./characterslist/characterslist.module').then( m => m.CharacterslistPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/characterslist/characterslist.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-characterslist',
+  templateUrl: './characterslist.page.html',
+  styleUrls: ['./characterslist.page.scss'],
+})
+export class CharacterslistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
